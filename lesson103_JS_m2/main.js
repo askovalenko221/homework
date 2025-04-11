f8b8283387e1dcf154d03690ae82a4b974994e12
askovalenko221 <askovalenko221@gmail.com>
2025-04-11 16:33:42 +0300

    sdsds

--- lesson103_JS_m2/main.js
+++ lesson103_JS_m2/main.js
@@ -1,8 +1,38 @@
 const foo = 'Hello '
 const user = {
-        name: 'Aleksander',
-        age: 36,
-        isAdmin: true
+    name: 'Aleksander',
+    age: 36,
+    isAdmin: true
 }
 
 console.log(foo + user.name)
+
+
+const users = [
+    {
+        name: 'Pavel',
+        age: 21,
+        massa: '54 kg'
+        isAdmin: true
+    }
+    {
+        name: 'Denis',
+        age: 29,
+        massa: '70 kg'
+        isAdmin: true
+    }
+    {
+        name: 'Egor',
+        age: 36,
+        massa: '67 kg'
+        isAdmin: false
+    }
+]
+
+const countUsers = 0
+for (let i = 0; i < users.length; i++) {
+    if (users[i].isAdmin == true)
+    const countUsers = countUsers +1
+}
+
+console.log(countUsers)
