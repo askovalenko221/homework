98c589383d38761b8bb2c87ca4c57ab54748ba7b
askovalenko221 <askovalenko221@gmail.com>
2025-04-24 17:34:22 +0300

    v3

--- maket_№3_Wake&Bake/js/main.js
+++ maket_№3_Wake&Bake/js/main.js
@@ -92,16 +92,35 @@
 
         el.addEventListener('click', (e) => {
 
+            // для атоматического сворачивания всех кроме активного элемента V2
+            // если не нужно, то строки ниже убрать
+
+            const accordionList = e.currentTarget
+            const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+            const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+            console.log(accordionOpenedItem)
+            console.log(accordionOpenedContent)
+            //  V1
             const accordionControl = e.target.closest('.accordion-list__control');
             if (!accordionControl) return
             const accordionItem = accordionControl.parentElement;
             const accordionContent = accordionControl.nextElementSibling;
 
+
+            // V2
+            if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+                accordionOpenedItem.classList.remove('accordion-list__item--opened');
+                accordionOpenedContent.style.maxHeight = null;
+            }
+            // V2
+
+
             accordionItem.classList.toggle('accordion-list__item--opened');
 
             if (accordionItem.classList.contains('accordion-list__item--opened')) {
                 accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
-            } else{
+            } else {
                 accordionContent.style.maxHeight = null;
             }
         })
