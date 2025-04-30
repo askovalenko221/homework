1c587b2ff59d9dc0143e539aaf5a8cca2b12750d
askovalenko221 <askovalenko221@gmail.com>
2025-04-30 12:41:32 +0300

    contact final

--- maket_№3_Wake&Bake/js/main.js
+++ maket_№3_Wake&Bake/js/main.js
@@ -205,5 +205,13 @@
 
     });
 
+    // Маска для номера телефона
+
+    const telInputs = document.querySelectorAll('input[type="tel"]')
+
+    const im = new Inputmask('+7 (999) 999-99-99')
+
+    im.mask(telInputs)
+
 })()
 
